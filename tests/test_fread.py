f2c404120e69d1394046c0149b4ea0a8226d7e9f
xaviermiles <xaviermiles377@gmail.com>
2022-04-14 15:17:50 +1200

    test_fread: add fixtures and separate type/eq checks
    This is now meant to check all 3 samples for types of main parts (and
    hopefully other stuff given the fixture), but the third sample was
    raising a DNS issue on my Python but not browser so it only does 2.
    
    It also checks the equality of the SdmxJsonDataMessage instances
    returned by fread_json from remote and local paths, but the __eq__()
    method isn't implemented yet, so this is a known fail.

--- tests/test_fread.py
+++ tests/test_fread.py
@@ -1,26 +1,52 @@
 import os
 
+import pytest
 import requests
 
 from sdmx_dt import fread
 from tests import DATA_DIR
 
+sdmx_json_samples_url = (
+    "https://github.com/sdmx-twg/sdmx-json/raw/master/data-message/samples/"
+)
 
-def test_fread_json_samples():
-    path_r = "https://github.com/sdmx-twg/sdmx-json/raw/master/data-message/samples/agri.json"
-    message_remote = fread.fread_json(path_r)
 
-    path_l = os.path.join(DATA_DIR, path_r.split("/")[-1])
-    r = requests.get(path_r, stream=True)
-    with open(path_l, "w") as f:
-        f.write(r.content.decode())
-    message_local = fread.fread_json(path_l, is_url=False)
+@pytest.fixture
+def sdmx_json_sample_names():
+    return [
+        "agri.json",
+        "constructed-sample-full.json",
+        # "generated-sample.json",  # FIXME: why is this raising DNS problems?
+    ]
 
-    # TODO: implement __eq__() ?
-    # assert message_remote == message_local
 
-    assert isinstance(message_remote, fread.SdmxJsonDataMessage)
-    # Check for empty top-level objects?
-    assert isinstance(message_remote.meta, fread.SdmxJsonMeta)
-    assert isinstance(message_remote.data, fread.SdmxJsonData)
-    assert message_remote.errors is None  # this is missing in agri.json
+@pytest.fixture
+def sdmx_json_sample_messages(sdmx_json_sample_names):
+    return [fread.fread_json(sdmx_json_samples_url + f) for f in sdmx_json_sample_names]
+
+
+@pytest.fixture
+def local_sdmx_json_sample_messages(sdmx_json_sample_names):
+    local_messages = []
+    for name in sdmx_json_sample_names:
+        path = os.path.join(DATA_DIR, name)
+        r = requests.get(sdmx_json_samples_url + name)
+        with open(path, "w") as f:
+            f.write(r.content.decode())
+        local_messages.append(fread.fread_json(path, is_url=False))
+    return local_messages
+
+
+def test_fread_json_local_and_remote_eq(
+    sdmx_json_sample_messages, local_sdmx_json_sample_messages
+):
+    assert sdmx_json_sample_messages == local_sdmx_json_sample_messages
+
+
+def test_fread_json_types(sdmx_json_sample_messages):
+    for msg in sdmx_json_sample_messages:
+        assert isinstance(msg, fread.SdmxJsonDataMessage)
+        # Check for empty top-level objects?
+        assert isinstance(msg.meta, fread.SdmxJsonMeta) or msg.meta is None
+        assert isinstance(msg.data, fread.SdmxJsonData) or msg.data is None
+        assert isinstance(msg.errors, fread.SdmxJsonErrors) or msg.errors is None
