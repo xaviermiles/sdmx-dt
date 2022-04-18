a1ce30cc68426a195627ab790ade0f4a256f234c
xaviermiles <xaviermiles377@gmail.com>
2022-04-18 12:38:44 +1200

    pytest.mark.parametrize and clear test_data/ on every run
    parametrize() improves the flow of test functions and removes the need
    for the for loops. The test_data/ folder is now deleted on every pytest
    run to ensure repeatability of tests and that no old data is lingering.

--- tests/test_fread.py
+++ tests/test_fread.py
@@ -6,48 +6,43 @@ import requests
 from sdmx_dt import fread
 from tests import DATA_DIR
 
+# Commit 21d2034 is v1.0 of SDMX-JSON
 sdmx_json_samples_url = (
-    "https://github.com/sdmx-twg/sdmx-json/raw/master/data-message/samples/"
+    "https://raw.githubusercontent.com/sdmx-twg/sdmx-json/21d2034/data-message/samples/"
+)
+pytestmark = pytest.mark.parametrize(
+    "name",
+    [
+        "agri.json",
+        "exr/exr-action-delete.json",
+        "exr/exr-cross-section.json",
+    ],
 )
 
 
 @pytest.fixture
-def sdmx_json_sample_names():
-    return [
-        "agri.json",
-        "constructed-sample-full.json",
-        # "generated-sample.json",  # FIXME: why is this raising DNS problems?
-    ]
+def sdmx_json_msg_remote(name):
+    return fread.fread_json(sdmx_json_samples_url + name)
 
 
 @pytest.fixture
-def sdmx_json_sample_messages(sdmx_json_sample_names):
-    return [fread.fread_json(sdmx_json_samples_url + f) for f in sdmx_json_sample_names]
+def sdmx_json_msg_local(name):
+    path = os.path.join(DATA_DIR, name.split("/")[-1])
+    r = requests.get(sdmx_json_samples_url + name)
+    with open(path, "w") as f:
+        f.write(r.content.decode())
+    return fread.fread_json(path, is_url=False)
 
 
-@pytest.fixture
-def local_sdmx_json_sample_messages(sdmx_json_sample_names):
-    local_messages = []
-    for name in sdmx_json_sample_names:
-        path = os.path.join(DATA_DIR, name)
-        r = requests.get(sdmx_json_samples_url + name)
-        with open(path, "w") as f:
-            f.write(r.content.decode())
-        local_messages.append(fread.fread_json(path, is_url=False))
-    return local_messages
+def test_fread_json_local_and_remote_eq(sdmx_json_msg_remote, sdmx_json_msg_local):
+    assert sdmx_json_msg_remote == sdmx_json_msg_local
 
 
-def test_fread_json_local_and_remote_eq(
-    sdmx_json_sample_messages, local_sdmx_json_sample_messages
-):
-    assert sdmx_json_sample_messages == local_sdmx_json_sample_messages
-
-
-def test_fread_json_types(sdmx_json_sample_messages):
-    for msg in sdmx_json_sample_messages:
-        assert isinstance(msg, fread.SdmxJsonDataMessage)
-        assert isinstance(msg.meta, fread.SdmxJsonMeta) or msg.meta is None
-        assert isinstance(msg.data, fread.SdmxJsonData) or msg.data is None
-        assert isinstance(msg.errors, list) and (
-            len(msg.errors) == 0 or isinstance(msg.errors[0], fread.SdmxJsonError)
-        )
+def test_fread_json_types(name, sdmx_json_msg_remote):
+    msg = sdmx_json_msg_remote  # shorter alias
+    assert isinstance(msg, fread.SdmxJsonDataMessage)
+    assert isinstance(msg.meta, fread.SdmxJsonMeta) or msg.meta is None
+    assert isinstance(msg.data, fread.SdmxJsonData) or msg.data is None
+    assert isinstance(msg.errors, list) and (
+        len(msg.errors) == 0 or isinstance(msg.errors[0], fread.SdmxJsonError)
+    )
