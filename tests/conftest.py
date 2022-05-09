3f322e770cce4f2379888b7d332b570ee87db56e
xaviermiles <xaviermiles377@gmail.com>
2022-05-09 18:57:10 +1200

    Fix: helper.check_dt_Frames_eq checks column order

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,14 @@ class Helpers:
     def check_dt_Frames_eq(f1, f2):
         assert isinstance(f1, Frame) and isinstance(f2, Frame)
 
-        # Using to_dict() method since __eq__() method doesn't seem to work
-        assert f1.to_dict() == f2.to_dict()
+        # Note: Simply using __eq__() method doesn't seem to work
+        # Check order/type of columns
+        assert f1.keys() == f2.keys()
         assert f1.types == f2.types
+
+        # Check datapoints
+        assert f1.to_csv() == f2.to_csv()
+        assert f1.to_dict() == f2.to_dict()
 
 
 @pytest.fixture
