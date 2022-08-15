5dc48b1751aaddba144d21db3589282d2a91a08d
xaviermiles <xaviermiles377@gmail.com>
2022-08-15 15:40:09 +1200

    Add some placeholder methods to represent relationships

--- src/sdmx_dt/information_model/structure.py
+++ src/sdmx_dt/information_model/structure.py
@@ -1,15 +1,23 @@
 from abc import ABC
+from typing import Optional
 
 from sdmx_dt.information_model.base import MaintainableArtefact
+from sdmx_dt.information_model.codelist import CodeList
 
 
 class Representation:
-    pass
+    def placeholder(self, enumerated: Optional[CodeList]) -> None:
+        # Component has one-way association "enumerated" to CodeList
+        pass
 
 
 class Component:
     def __init__(self, has_local_repr: bool = True) -> None:
-        self.local_representation = Representation() if has_local_repr else None
+        self.local_representation = Representation(None) if has_local_repr else None
+
+    def placeholder(self, concept_identity: Optional[CodeList]) -> None:
+        # Component has one-way association "concept_identity" to CodeList
+        pass
 
 
 class ComponentList:
